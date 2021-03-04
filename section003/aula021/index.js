642a17d401005315f36901c540b971ff05b7b96b
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-03-04 16:14:03 -0300

    :books: Add aula021

---
+++ section003/aula021/index.js
@@ -0,0 +1,24 @@
+// Comparison operators
+
+/*
+  As the name suggests, these operator compare two values and return true or
+  false. They are:
+
+  == - Equity
+  === - Strict equity (checks value and type)
+  != - Difference (checks if one value is different from another)
+  !== - Strict difference (same as strict equity)
+  > - Checks if is greater than the compared value
+  < - Checks if is less than the compared value
+  >= - Checks if is equal or greater than the compared value
+  <= - Checks if is equal or less then the compared value
+
+  Ex:
+    const a = 2
+    const b = '2'
+
+    console.log(a == b) // -> True
+    console.log(a === b) // -> False
+    console.log(3 >= a) // -> True
+    console.log(3 <= a ) // -> False
+*/
