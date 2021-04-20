43c402a20da164558d780305f95298ee141ca584
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-04-20 16:23:14 -0300

    :books: Add aula 034

---
+++ section003/aula034/index.js
@@ -0,0 +1,23 @@
+// For loops
+
+/*
+  Instead of repeating ourselves, we can create a loop that will repeat it's
+  code until the given condidition is evaluated falsey.
+
+  for (let i = 0; i < 5; i++) {
+    console.log(i)
+  }
+
+  // -> 0, 1, 2, 3, 4
+
+  We can use the same logic to iterate over arrays, for example.
+
+  const gamesArray = ['Final Fantasy VII', 'Resident Evil 4', 'Devil May Cry 5',
+ 'Bloodborne', 'Dark Souls III' ]
+
+  for (let i = 0; i < gamesArray.length; i++) {
+    console.log(`The game at index ${i} is ${gamesArray[i]}.`)
+  }
+
+  // -> The game at index 0 is Final Fantasy VII, The game at...
+*/
