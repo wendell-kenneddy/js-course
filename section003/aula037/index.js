86a9936e2f968738d1dc034fef8b23accc76ebea
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-04-21 15:01:11 -0300

    :books: Add aula037

---
+++ section003/aula037/index.js
@@ -0,0 +1,18 @@
+// For..in
+
+/*
+  The for in loop iterates over the properties of a object, like the indexes of
+  a array or the keys of the object. It returns only the key, not the value.
+
+  const myObj = {
+    name: 'Wendell',
+    nick: 'Kenneddy',
+    age: 16
+  };
+
+  for (let key in myObj) {
+    console.log(`The value of the key ${key} is ${myObj[key]}.`);
+  }
+
+  // -> The value of the key name is Wendell, The value of the key...
+*/
