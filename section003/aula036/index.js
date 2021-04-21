01b589132207e7770dfb16586679588fb84aa815
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-04-21 14:36:34 -0300

    :books: Add aula036

---
+++ section003/aula036/index.js
@@ -0,0 +1,16 @@
+// Document Object Model
+
+/*
+  DOM is the browser API that allow us to manipulate the html document using
+  Javascript. DOM is a tree-like data structure, so elements can have nodes
+  (childs) and parents.
+
+  const x = document.getElementById('demo');
+  console.log(x);
+
+  In the example above, we are selecting the element with the ID 'demo' in the
+  html document, and showing it's content in the console.
+
+  Elements in the DOM tree are treated like objects, so they have methods as
+  well, such as innerHTML, innerText, style and so on.
+*/
