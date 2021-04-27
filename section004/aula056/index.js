31f204037cb20a540c7c208a2f8ed3eacc58a88c
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-04-27 15:14:49 -0300

    :books: Add aula 056

---
+++ section004/aula056/index.js
@@ -0,0 +1,11 @@
+// IIFE (Immediately Invoked Function Expression)
+
+/*
+  IIFE's are functions called right after being created. IIFE functions don't
+  pollute the global scope.
+
+  const example = (function (name, nickname) {
+    console.log(`Meu nome é ${name} ${nickname}.`)
+  })('Wendell', 'Kenneddy');
+
+*/
