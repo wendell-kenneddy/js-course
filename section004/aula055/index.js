e2453b46c40f5ee3fb637d29a761438e509d3b6d
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-04-27 14:52:16 -0300

    :books: Add aula 055

---
+++ section004/aula055/index.js
@@ -0,0 +1,16 @@
+// Callback functions
+
+/*
+  Callback functions are functions passed to another function as argument, to be
+  executed completing a certain routine or action (an interval, for example).
+
+  const sayHi = callback => {
+    if (callback) callback();
+    console.log('Hi!');
+  }
+
+  sayHi(() => {
+    console.log('Hello!');
+  }) // -> Hello!...Hi!
+*/
+
