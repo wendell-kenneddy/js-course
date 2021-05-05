51685be550e36715e936872e57676eec2a25770b
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-05-05 15:25:16 -0300

    :books: Add aula064

---
+++ section005/aula064/index.js
@@ -0,0 +1,13 @@
+// Concatenate Arrays
+
+/*
+  To concatenate arrays we can use the native method concat(), that pushes to
+  the target array the given arguments, or we can use the spread operator.
+
+  const arr = ['Orange', 'Apple', 'Lemon']
+  const arr2 = ['Bread', 'Pasta'];
+  const arr3 = arr.concat(arr2);
+  const arr4 = [...arr, ...arr2];
+  console.log(arr3) // -> Orange, Apple, Lemon, Bread, Pasta
+  console.log(arr4) // -> Orange, Apple, Lemon, Bread, Pasta
+*/
