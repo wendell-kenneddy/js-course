469598f9fc4b83c1eb15c28612d75c2a0b39dafc
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-05-05 14:35:22 -0300

    :books: Add aula062

---
+++ section005/aula062/index.js
@@ -0,0 +1,25 @@
+// Arrays revision
+
+/*
+  Arrays are a data structure based of indexed items in a sort of list. We can
+  use some methods in arrays, the basics are push, shift, unshift, slice, split
+  and join.
+
+  const myArray = ['Orange', 'Apple', 'Lemon'];
+  myArray.push('Grape');
+  console.log(myArray); // -> Orange, Apple, Lemon, Grape
+
+  myArray.shift();
+  console.log(myArray); // -> Apple, Lemon, Grape
+
+  myArray.unshift('Orange');
+  console.log(myArray); // -> Orange, Apple, Lemon, Grape
+
+  console.log(myArray.slice(0, -2)) // -> Orange, Apple
+
+  console.log(myArray.join('--')) // -> Orange--Apple--Lemon--Grape
+
+  const myString = 'Alone in a room';
+  const newArray = myString.split(' ');
+  console.log(newArray) // -> Alone, in, a, room
+*/
