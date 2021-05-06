fec09078965333458f33604bac7d1cd74060fea9
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-05-06 15:37:14 -0300

    :books: Add aula069

---
+++ section005/aula069/index.js
@@ -0,0 +1,11 @@
+// forEach
+
+/*
+  The forEach method executes a function to each element of an array. It
+  receives a callback function, that can receive the value, the index and the
+  source array as parameters.
+
+  const numArray = [10, 3, 6, 9, 15, 21];
+  numArray.forEach(e => console.log(e * 2)); // -> 20, 6, 12, 18, 30, 42
+*/
+
