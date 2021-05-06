56e95169c5de3d161d40e4108c84c35f0612e4c4
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-05-06 15:20:07 -0300

    :books: Add aula068

---
+++ section005/aula068/index.js
@@ -0,0 +1,11 @@
+// Filter + map + reduce
+
+/*
+  const numArray = [10, 27, 38, 91, 80, 33, 41, 56, 72, 89, 100];
+  const doubledEvenSum =
+    numArray.filter(e => e %2 == 0)
+      .map(e => e * 2)
+      .reduce((ac, e) => ac + e);
+
+  console.log(doubledEvenSum); // -> 712
+*/
