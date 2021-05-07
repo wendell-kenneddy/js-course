4f155ef09a3527a356637b4fb846f399839160c5
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-05-07 16:38:40 -0300

    :books: Add aula073

---
+++ section006/aula073/index.js
@@ -0,0 +1,19 @@
+// Useful Object methods
+
+/*
+  Object.assign({...}) -> copies the key-value pairs of an object to another object,
+  without making both reference the same memory address.
+
+  Object.values({...}) -> returns an array containing the values of the keys in the
+  object.
+
+  Object.getOwnPropertyDescriptor({...}, 'prop') -> returns the descriptor of
+  the desired property.
+
+  Object.entries({...}) -> returns an array containing arrays containing two
+  indexes, being the first the key name of the entry, and the second index the
+  value of the key.
+
+  Object.freeze({...}) -> prevents the object of being modifified, reconfigured,
+  deleted and etc.
+*/
