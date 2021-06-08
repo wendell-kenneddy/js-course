53d6479dd37cf1b7326bc5228b27e7a597f24e2c
Wendell Kenneddy <wkenneddy505@gmail.com>
2021-06-08 17:03:01 -0300

    :books: Add aula 099

---
+++ section010/aula099/main.js
@@ -0,0 +1,7 @@
+// Example of module importing using absolute path
+
+/*
+  const path = require('path');
+  const mult = require(path.resolve(__dirname, 'sample', 'module.js'));
+  console.log(mult(10, 5)); // -> 50
+*/
